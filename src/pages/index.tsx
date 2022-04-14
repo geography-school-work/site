2c4429fccb3d059f4bda205ad8aefb8ea4658428
End313234 <end313234@protonmail.com>
2022-04-13 22:29:17 -0300

    Add form to the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,95 @@
 import type { NextPage } from "next";
+import {
+	Button,
+	FormControl,
+	FormErrorMessage,
+	FormLabel,
+	Input,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalFooter,
+	ModalHeader,
+	ModalOverlay,
+	propNames,
+	useDisclosure,
+} from "@chakra-ui/react";
+import { Field, Form, Formik } from "formik";
 import { HeaderComponent } from "../web/components/header";
 import { HomeStyles } from "../styles/home";
 
 const Home: NextPage = () => {
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	const validateField = (value) => {
+		if (!value) {
+			return "This field is required";
+		}
+		return "";
+	};
+
 	return (
 		<>
 			<HeaderComponent />
 			<HomeStyles>
-				<h2>
-					Uma empresa sustentável, feita para você!
-				</h2>
-				<p>
-					A HortaSua tem como proposta pomover uma maior sustentabilidade,
-					permitindo ao consumidor uma horta auto-regulável em sua própria
-					casa ou apartamento.
-				</p>
+				<div id="presentation">
+					<h2>Uma empresa sustentável, feita para você!</h2>
+					<p>
+						A HortaSua tem como proposta pomover uma maior sustentabilidade,
+						permitindo ao consumidor uma horta auto-regulável em sua própria
+						casa ou apartamento.
+					</p>
+				</div>
+				<div id="form-invite">
+					<h2>Gostou do que leu? Então nos dê sua opinião!</h2>
+					<p>
+						Responda ao formulário abaixo para nos dizer um pouco mais sobre a
+						sua experiência com a nossa empresa!
+					</p>
+					<button onClick={onOpen}>Responder ao formulário</button>
+				</div>
 			</HomeStyles>
+			<Modal isOpen={isOpen} onClose={onClose} isCentered>
+				<ModalOverlay />
+				<ModalContent width="90%">
+					<ModalHeader>Formulário</ModalHeader>
+					<ModalCloseButton />
+					<ModalBody>
+						<Formik
+							initialValues={{}}
+							onSubmit={(values, action) => {
+								console.log(values);
+							}}
+						>
+							{(props) => (
+								<Form>
+									<Field mb={5} name="Alguma coisa">
+										{({ field, form }) => (
+											<FormControl
+												isInvalid={form.errors.name && form.touched.name}
+											>
+												<FormLabel htmlFor="Alguma coisa">Alguma coisa</FormLabel>
+												<Input {...field} placeholder="Alguma coisa" />
+												<FormErrorMessage>{form.errors.name}</FormErrorMessage>
+											</FormControl>
+										)}
+									</Field>
+									<Button
+										mt={4}
+										mb={4}
+										colorScheme="teal"
+										isLoading={props.isSubmitting}
+										type="submit"
+									>
+										Enviar
+									</Button>
+								</Form>
+							)}
+						</Formik>
+					</ModalBody>
+				</ModalContent>
+			</Modal>
 		</>
 	);
 };
--- src/styles/home.ts
+++ src/styles/home.ts
@@ -6,22 +6,42 @@ export const HomeStyles = styled.main`
     justify-content: center;
     flex-direction: column;
     text-align: center;
+    gap: 1rem;
 
-    > h2 {
-        font-family: "Ubuntu";
-        margin-top: 7vh;
-        margin-bottom: 0;
-        width: 25vw;
-        font-size: 1.5rem;
-        font-weight: bold;
-        min-width: 18rem;
-        margin-bottom: 1rem;
+    > #presentation,
+    > #form-invite {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+
+        > h2 {
+            font-family: "Ubuntu";
+            margin-top: 7vh;
+            margin-bottom: 0;
+            width: 25vw;
+            font-size: 1.5rem;
+            font-weight: bold;
+            min-width: 18rem;
+            margin-bottom: 1rem;
+        }
+
+        > p {
+            font-size: 1.1rem;
+            width: 50vw;
+            min-width: 18rem;
+            font-family: "Poppins";
+        }
     }
 
-    > p {
-        font-size: 1.1rem;
-        width: 50vw;
-        min-width: 18rem;
+    > #form-invite > button {
+        margin: 1rem 0;
+        background-color: ${({ theme }) => theme.GREEN};
+        border: none;
+        border-radius: 1.4rem;
         font-family: "Poppins";
+        font-weight: bold;
+        padding: 1.25rem;
+        color: ${({ theme }) => theme.WHITE}
     }
 `;
